27bf3129c07d60372ae387c226da887fb504895c
XIPA2BUDHAYA <111984481+XIPA2BUDHAYA@users.noreply.github.com>
2022-09-17 08:32:32 +0000

    Add files via upload

--- js/music.js
+++ js/music.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var pth = path.join(__dirname);
 
-function williamns() {
+async function williamns() {
     fs.readdir(`${pth}/music/`, (err, files) => {
         new Audio(files[Math.floor(Math.random() * files.length)]).play();
     })
